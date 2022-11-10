aa3fbcef419efccd98b56a134732c2d177b7fa51
Igor Brizack <igor_brizack@hotmail.com>
2022-11-10 15:08:49 -0300

    feat: trying to pass in the github tests

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -3,5 +3,4 @@ db.produtos.updateMany({ "valoresNutricionais.2.percentual": { $gt: 20, $lt: 40 
     tags: { $each: ["contém sódio"] },
   },
 });
-
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
